dd78fda1ea2392e04c7a75867d8f88a9b1193fa6
Diego Alor <diegoalor@upeu.edu.pe>
2020-12-17 16:50:57 -0500

    exa3-frontend-diego-alor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ListarComponent } from './components/roles/listar/listar.component';
+import {AuthrutasGuard} from './../app/services/guards/authrutas.guard';
+import { RoleGuard } from './services/guards/role.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path: 'listar', component: ListarComponent, canActivate:[AuthrutasGuard, RoleGuard], data:{role:'ADMIN'}},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,50 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { MenuComponent } from './components/template/menu/menu.component';
+import { ListarComponent } from './components/roles/listar/listar.component';
+import { UpdateComponent } from './components/roles/update/update.component';
+import { RolService } from './services/rol.service';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './interceptores/interceptor.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InterceptorErrorService } from './interceptores/interceptor.service.spec';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VentasComponent,
+    FooterComponent,
+    MenuComponent,
+    ListarComponent,
+    UpdateComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RolService,AuthService,
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi:true
+    },
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass: InterceptorErrorService,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
